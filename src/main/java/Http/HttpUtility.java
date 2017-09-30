b37164bfa32e88a8d078d101b12e4cb998016624
mikk sillamaa <mikk.sillamaa@gmail.com>
2017-09-30 17:34:53 +0300

    tests

--- src/main/java/Http/HttpUtility.java
+++ src/main/java/Http/HttpUtility.java
@@ -1,33 +1,10 @@
 package Http;
 
 import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class HttpUtility {
 
-    public static HttpURLConnection makeHttpGetRequest(String url) {
+    public static HttpURLConnection makeHttpGetRequest(String request) {
         return null;
     }
-
-    public static boolean urlNotNull(String url) {
-        return false;
-    }
-
-    public static boolean urlNotEmpty(String url) {
-        return false;
-    }
-
-    public static boolean urlCorrectStructure(String url) {
-        return false;
-    }
-
-    public static boolean jsonFileCanBeDownloaded() {
-        return false;
-    }
-
-    public static boolean jsonFileContainsNecessaryData() {
-        return false;
-    }
-
-
 }
--- src/test/java/Http/HttpUtilityTest.java
+++ src/test/java/Http/HttpUtilityTest.java
@@ -4,71 +4,53 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.fail;
+
+import static org.junit.Assert.*;
 
 public class HttpUtilityTest {
 
     @Test
-    public void testHttpSuccessful() throws IOException {
+    public void httpConnectionSuccessfulTest() throws IOException {
         try {
             String requestUrl = "<API REQUEST URL>";
             HttpURLConnection con = HttpUtility.makeHttpGetRequest(requestUrl);
             assertEquals(con.getResponseCode(), 200);
         } catch (IOException ioq) {
-            fail("HTTP connection threw error:" + ioq.getLocalizedMessage());
+            fail("HTTP connection threw error: " + ioq.getLocalizedMessage());
         }
-
     }
 
     @Test
-    public void testUrlNotNull() {
-        assertEquals(HttpUtility.urlNotNull("http://neti.ee"), true);
+    public void httpRequestResultTypeJson() {
+        try {
+            String requestUrl = "<API REQUEST URL>";
+            HttpURLConnection con = HttpUtility.makeHttpGetRequest(requestUrl);
+            assertEquals(con.getContentType(), "json");
+        } catch (Exception ioq) {
+            fail("HTTP connection threw error: " + ioq.getLocalizedMessage());
+        }
     }
 
     @Test
-    public void testUrlNotEmpty() {
-        assertEquals(HttpUtility.urlNotEmpty(""), true);
+    public void httpRequestResultTypeNotNull() {
+        try {
+            String requestUrl = "<API REQUEST URL>";
+            HttpURLConnection con = HttpUtility.makeHttpGetRequest(requestUrl);
+            assertEquals(con.getContentType() != null, true);
+        } catch (Exception ioq) {
+            fail("HTTP connection threw error: " + ioq.getLocalizedMessage());
+        }
     }
 
     @Test
-    public void testUrlCorrectStructure() {
-        assertEquals(HttpUtility.urlCorrectStructure("api.openweathermap.org/data/2.5/forecast?q=London,us"),true);
+    public void httpRequestResultTypeNotEmpty() {
+        try {
+            String requestUrl = "<API REQUEST URL>";
+            HttpURLConnection con = HttpUtility.makeHttpGetRequest(requestUrl);
+            assertEquals(con.getContentType().equals(""), true);
+        } catch (Exception ioq) {
+            fail("HTTP connection threw error: " + ioq.getLocalizedMessage());
+        }
     }
-
-    @Test
-    public void testHttpCorrectContentType() throws IOException {
-        String requestUrl = "<API REQUEST URL>";
-        HttpURLConnection con = HttpUtility.makeHttpGetRequest(requestUrl);
-        assertEquals(con.getContentType(), "json");
-    }
-
-    @Test
-    public void testHttpContentNotNull() throws IOException {
-        String requestUrl = "<API REQUEST URL>";
-        HttpURLConnection con = HttpUtility.makeHttpGetRequest(requestUrl);
-        assertNotEquals(con.getInputStream(), null);
-    }
-
-    @Test
-    public void testHttpContentNotEmpty() throws IOException {
-        String requestUrl = "<API REQUEST URL>";
-        HttpURLConnection con = HttpUtility.makeHttpGetRequest(requestUrl);
-        assertNotEquals(con.getInputStream(), "");
-    }
-
-    @Test
-    public void testJsonFileCanBeDownloaded() throws IOException {
-        HttpUtility http = new HttpUtility();
-        assertEquals(http.jsonFileCanBeDownloaded(), true);
-    }
-
-    @Test
-    public void testJsonFileContainsNecessaryData() throws IOException {
-        HttpUtility http = new HttpUtility();
-        assertEquals(http.jsonFileContainsNecessaryData(), true);
-    }
-
 
 }
